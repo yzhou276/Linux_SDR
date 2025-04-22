97a37d88b986743f19a06196eead0aa837ee9897
Yuchen Zhou <yzhou276@jh.edu>
2025-04-21 23:36:24 -0400

    add the basic framework

--- web/cgi-bin/udpFifoStreamer.c
+++ web/cgi-bin/udpFifoStreamer.c
@@ -13,7 +13,150 @@
 /* Include */
 #include "udpFifoStreamer.h"
 
-/* Function Prototype */
+/** Global Variables */
+volatile unsigned int *axi_fifo_base = NULL;    // Pointer to the AXI FIFO base address
+char *dest_ip = DEFAULT_DEST_IP;                // Destination IP address
+int dest_port = DEFAULT_UDP_DEST_PORT;          // Destination UDP port
+int timeout = 0;                                // Timeout in seconds (default: infinite)
+dataPacket packet;                              // Data packet structure
+sem_t data_ready;                               // Semaphore for data ready signal
+pthread_mutex_t mutex;                          // Mutex for thread synchronization
+volatile sig_atomic_t terminate = 0;            // Termination flag
+
+/** Thread Tasks */
+void *fifoReaderTask(void *arg);
+void *udpSenderTask(void *arg);
+
+int main(int argc, char const *argv[])
+{
+    int opt = 0;
+    // Check command line arguments
+    while ((opt = getopt(argc, (char * const *)argv, "i:p:t:h")) != -1) {
+        switch (opt) {
+            case 'i':
+                dest_ip = optarg; break;
+            case 'p':
+                dest_port = atoi(optarg); break;
+            case 't':
+                timeout = atoi(optarg); break;
+            case 'h':
+                usage(argv[0]); return 0;
+            default:
+                usage(argv[0]); return -1;
+        }
+    }
+
+    // check if the arguments are valid
+    if(timeout < 0) {
+        fprintf(stderr, "Invalid timeout value: %d\n", timeout);
+        return -1;
+    }
+    if(dest_port < 0 || dest_port > 65535) {
+        fprintf(stderr, "Invalid port value: %d\n", dest_port);
+        return -1;
+    }
+    if(inet_pton(AF_INET, dest_ip, NULL) <= 0) {
+        fprintf(stderr, "Invalid IP address: %s\n", dest_ip);
+        return -1;
+    }
+
+    // initialize the semaphore and mutex
+    if(sem_init(&data_ready, 0, 0) != 0) {
+        perror("Failed to initialize semaphore");
+        return -1;
+    }
+    if(pthread_mutex_init(&mutex, NULL) != 0) {
+        perror("Failed to initialize mutex");
+        sem_destroy(&data_ready);
+        return -1;
+    }
+
+    pthread fifoReaderThread;
+    pthread udpSenderThread;
+    // create the FIFO reader thread
+    if(pthread_create(&fifoReaderThread, NULL, fifoReaderTask, NULL) != 0) {
+        perror("Failed to create FIFO reader thread");
+        return -1;
+    }
+    // create the UDP sender thread
+    if(pthread_create(&udpSenderThread, NULL, udpSenderTask, NULL) != 0) {
+        perror("Failed to create UDP sender thread");
+        pthread_cancel(fifoReaderThread); // cancel the FIFO reader thread
+        return -1;
+    }
+
+    // wait for the threads to finish
+    pthread_join(fifoReaderThread, NULL);
+    pthread_join(udpSenderThread, NULL);
+
+    sem_destroy(&data_ready); // destroy the semaphore
+    pthread_mutex_destroy(&mutex); // destroy the mutex
+
+    // unmap the AXI FIFO base address
+    if(axi_fifo_base != NULL) {
+        munmap((void *)axi_fifo_base, 4096); // unmap the AXI FIFO base address
+    }
+
+    return 0;
+}
+
+void* fifoReaderTask(void *arg)
+{
+    // get a pointer to the AXI FIFO base address
+    axi_fifo_base = get_pointer_to_axi_fifo();
+    if (axi_fifo_base == NULL) 
+    {
+        fprintf(stderr, "Failed to get AXI FIFO base address\n");
+        pthread_exit(NULL);
+    }
+
+
+
+}
+
+void* udpSenderTask(void* arg)
+{
+    // create a UDP socket
+    int sockfd = socket(AF_INET, SOCK_DGRAM, 0);
+    if (sockfd < 0) {
+        perror("Error creating socket");
+        pthread_exit(NULL);
+    }
+
+    // create a sockaddr_in structure to hold the server address
+    struct sockaddr_in server_addr;
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(dest_port);
+    if (inet_pton(AF_INET, dest_ip, &server_addr.sin_addr) <= 0) {
+        perror("Invalid IP address or IP Address not supported");
+        close(sockfd);
+        pthread_exit(NULL);
+    }
+
+    // send packets to the server
+    while (!terminate) {
+        sem_wait(&data_ready); // wait for data to be ready
+
+        // lock the mutex before accessing shared data
+        pthread_mutex_lock(&mutex);
+
+        ssize_t bytes_sent = sendto(sockfd, &packet, PACKET_SIZE, 0, (struct sockaddr *)&server_addr, sizeof(server_addr));
+        if (bytes_sent < 0) {
+            perror("Error sending packet");
+            pthread_mutex_unlock(&mutex); // unlock the mutex before exiting
+            close(sockfd);
+            pthread_exit(NULL);
+        }
+        printf("Sent packet ID: %d\n", packet.packetID);
+
+        pthread_mutex_unlock(&mutex); // unlock the mutex after sending the packet
+    }
+
+    close(sockfd); // close the socket
+    pthread_exit(NULL);
+}
+
 
 // the below code uses a device called /dev/mem to get a pointer to a physical
 // address.  We will use this pointer to read/write the custom peripheral
@@ -34,4 +177,47 @@ volatile unsigned int * get_a_pointer(unsigned int phys_addr)
 	volatile unsigned int *peripheral_base = (volatile unsigned int *)map_base; 
     close(mem_fd); // close the file descriptor after mapping
 	return (peripheral_base);
+}
+
+/**
+ * @brief Get a pointer to the AXI FIFO base address in user space
+ * 
+ * @return volatile unsigned int* Pointer to the AXI FIFO base address
+ */
+volatile unsigned int * get_pointer_to_axi_fifo()
+{
+    volatile unsigned int *axi_fifo_base = get_a_pointer(AXI4_STREAM_FIFO_BASE_ADDR);
+    if (axi_fifo_base == NULL) {
+        fprintf(stderr, "Failed to map AXI FIFO base address\n");
+        return NULL;
+    }
+    return axi_fifo_base;
+}
+
+void fifo_reset(volatile unsigned int *axi_fifo_base)
+{
+    // reset the FIFO by writing the reset key to the reset register
+    axi_fifo_base[AXI4_STREAM_FIFO_RDFR_OFFSET/4] = AXI4_STREAM_FIFO_RDFR_RESET_KEY;
+}
+
+unsigned int fifo_get_current_occupancy(volatile unsigned int *axi_fifo_base)
+{
+    // get the current occupancy of the FIFO by reading the occupancy register
+    unsigned int occupancy = axi_fifo_base[AXI4_STREAM_FIFO_RDFO_OFFSET/4];
+    return occupancy;
+}
+
+unsigned int fifo_get_data(volatile unsigned int *axi_fifo_base)
+{
+    // get the data from the FIFO by reading the data register
+    return axi_fifo_base[AXI4_STREAM_FIFO_RDFD_OFFSET/4];
+}
+
+void usage(const char *executableName)
+{
+    fprintf(stderr, "Usage: %s -i <IP address> -p <port> -t <timeout_second>\n\n", executableName);
+    fprintf(stderr, "  -i <IP address>      : Destination IP address (default: %s)\n\n", DEFAULT_DEST_IP);
+    fprintf(stderr, "  -p <port>            : Destination UDP port (default: %d)\n\n", DEFAULT_UDP_DEST_PORT);
+    fprintf(stderr, "  -t <timeout_second>  : Timeout in seconds (default: infinite)\n\n");
+    fprintf(stderr, "  -h                   : Show this help message\n\n");
 }
--- web/cgi-bin/udpFifoStreamer.h
+++ web/cgi-bin/udpFifoStreamer.h
@@ -32,17 +32,44 @@
  * @brief The transmit path of the IP core is not enabled.
  * PS is not able to transmit data to PL.
  */
-#define AXI4_STREAM_FIFO_BASE_ADDR 0x43c10000 // Base address of the AXI FIFO
+#define AXI4_STREAM_FIFO_BASE_ADDR       0x43c10000 // Base address of the AXI FIFO
 
 // refer to AXI4-Stream FIFO LogiCORE IP Product Guide (PG080) 
 // https://docs.amd.com/r/en-US/pg080-axi-fifo-mm-s/Register-Space
-#define AXI4_STREAM_FIFO_RDFR_OFFSET    0x18 // Receive data FIFO reset offset
-#define AXI4_STREAM_FIFO_RDFO_OFFSET    0x1C // Receive data FIFO occupancy offset
-#define AXI4_STREAM_FIFO_RDFD_OFFSET    0x20 // Receive data FIFO data offset
-#define AXI4_STREAM_FIFO_RLR_OFFSET     0x24 // Receive Length offset
+#define AXI4_STREAM_FIFO_RDFR_OFFSET     0x18 // Receive data FIFO reset offset
+#define AXI4_STREAM_FIFO_RDFO_OFFSET     0x1C // Receive data FIFO occupancy offset
+#define AXI4_STREAM_FIFO_RDFD_OFFSET     0x20 // Receive data FIFO data offset
+#define AXI4_STREAM_FIFO_RLR_OFFSET      0x24 // Receive Length offset
 
 #define AXI4_STREAM_FIFO_RDFR_RESET_MASK 0x000000FF // Reset mask for receive data FIFO
 #define AXI4_STREAM_FIFO_RDFR_RESET_KEY  0x000000A5 // Reset key for receive data FIFO
+
+/**
+ * @brief UDP default settings
+ */
+#define DEFAULT_UDP_DEST_PORT   25344           // Default UDP port
+#define DEFAULT_DEST_IP         "192.168.1.24"  // Default destination IP address
+
+/**
+ * @brief Data packet structure
+ */
+#define NUM_SAMPLES 256 // Number of samples in the packet
+typedef struct dataPacket
+{
+    uint32_t packetID;                  // Packet ID
+    int32_t  sdrData[NUM_SAMPLES];      // SDR data (256 samples)
+} dataPacket;
+
+#define PACKET_SIZE (sizeof(dataPacket)) // 1028 bytes
+
+/**  Function Prototype */
+volatile unsigned int * get_a_pointer(unsigned int phys_addr);
+volatile unsigned int * get_pointer_to_axi_fifo();
+void fifo_reset(volatile unsigned int *axi_fifo_base);
+unsigned int fifo_get_current_occupancy(volatile unsigned int *axi_fifo_base);
+unsigned int fifo_get_data(volatile unsigned int *axi_fifo_base);
+void usage(const char *executableName);
+
 
 
 #endif /* _UDP_FIFO_STREAMER_H_ */
