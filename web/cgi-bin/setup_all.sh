88765e4c4f53e47da0863574f1aac676eeff30b9
Yuchen Zhou <yzhou276@jh.edu>
2025-04-27 12:23:51 -0400

    update index.html and bash callback scripts

--- web/cgi-bin/setup_all.sh
+++ web/cgi-bin/setup_all.sh
@@ -1,18 +1,38 @@
 #!/bin/bash
+
+# default values
 STREAMING_APPLICATION=udpFifoStreamer2
+ip=192.168.1.3
+port=25344
+
+# Read POST data -- GPT suggested this
+read -n "$CONTENT_LENGTH" POST_DATA
+# Parse IP and PORT from POST
+ip=$(echo "$POST_DATA" | sed -n 's/.*ip=\([^&]*\).*/\1/p')
+port=$(echo "$POST_DATA" | sed -n 's/.*port=\([^&]*\).*/\1/p')
+
 killall -9 "$STREAMING_APPLICATION"
-echo "Content-type: text/html" # Tells the browser what kind of content to expect
-echo "" # An empty line. Mandatory, if it is missed the page content will not load
+
+# Echo HTTP headers
+echo "Content-type: text/html"
+echo ""
+
+# Echo HTML body
 echo "<html><body><p><em>"
-echo "loading PL...<br>"
+echo "Loading PL...<br>"
 fpgautil -b design_1_wrapper.bit.bin
 echo "</p></em><p>"
-echo "configuring Codec...<br>"
+
+echo "Configuring Codec...<br>"
 ./configure_codec.sh
 echo "</p>"
-echo "initializing the UDP Fifo streaming engine...<br>"
-echo "Destination IP address: 192.168.1.3<br>"
-echo "Destination UDP port: 25344<br>"
-./"$STREAMING_APPLICATION" &
-echo "<p><em>All Done!</em></p>" 
+
+echo "Initializing the UDP FIFO streaming engine...<br>"
+echo "UDP Destination IP address: $ip<br>"
+echo "UDP Destination UDP port: $port<br>"
+
+# Run the streaming application with user-supplied IP and PORT
+./"$STREAMING_APPLICATION" -i "$ip" -p "$port" > /dev/null 2>&1 & 
+
+echo "<p><em>All Done!</em></p>"
 echo "</body></html>"
